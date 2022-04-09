78bb04adef271523fe3b68fdb18b7d1932255eac
Ahmad Hassan Khan <98497427+mrahmadhassankhan@users.noreply.github.com>
2022-04-09 15:23:12 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ long long prim(int x)
 
 int main(void)
 {
-    clock_t tStart = clock();
+    clock_t tStart = clock();//time library used 
     int nodes, edges, x, y;
     long long weight, minimumCost;
     cout<<"Enter the no of nodes and then edges\n";
@@ -56,7 +56,7 @@ int main(void)
     // Selecting 1 as the starting node
     minimumCost = prim(1);
     cout <<  "The minimum cost of this tree is "<<  minimumCost << endl;
-    cout<<"Time taken: %.2fs\n"<< (double)(clock() - tStart)/CLOCKS_PER_SEC<<endl;
+    cout<<"Time taken: %.2fs\n"<< (double)(clock() - tStart)/CLOCKS_PER_SEC<<endl;//time library used 
 return 0;
     return 0;
 }
